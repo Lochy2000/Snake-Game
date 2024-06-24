bc7dad2cc4fd3adc3497e3cd3fffed380ec3ab78
lochy2000 <lochlann_oht@homtail.com>
2024-06-24 15:12:48 +0100

    added js file, added game-board element and draw function

---
+++ script.js
@@ -0,0 +1,9 @@
+// define HTML elements
+const board = document.getElementById('game-board');
+
+// adds game map, snake and food
+
+function draw () {
+    board.innerHTML = ''; // resets board to empty
+
+}
