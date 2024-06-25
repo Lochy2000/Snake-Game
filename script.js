d900fa1970c51597197df277c66f945ad80c4375
lochy2000 <lochlann_oht@homtail.com>
2024-06-25 09:50:24 +0100

    added snake element from css style, set its position on the board, then used appendchild to add it back to the board

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 const board = document.getElementById('game-board');
 
 // Define game variables
-let snake = [{x:10, y:10}]
+let snake = [{x:10, y:10}];
 
 // Adds game map, snake and food
 function draw () {
     board.innerHTML = ''; // resets board to empty
-
+    drawsnake();
 }
 
 // Draw snake 
-
 function drawsnake (){
-
+    snake.forEach ((segement) => {
+        const snakeElement = createGameElement ('div', 'snake');
+        setPosition (snakeElement, segement);
+        board.appendChild (snakeElement)
+    })
 }
