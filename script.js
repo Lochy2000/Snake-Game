a6d26f8d2bed7891df73f2bb494d31e78bce5cd8
lochy2000 <lochlann_oht@homtail.com>
2024-06-25 10:09:44 +0100

    set position of food and snake in x & y using the craeteGameElement function and the game varaibles position

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ function createGameElement (tag, className) {
     element.className = className;
     return element
 }
+
+// Set the position of snake or food 
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+
+}
