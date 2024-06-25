5dc7b12329524cb997df6b9617b2768cb318f7b3
lochy2000 <lochlann_oht@homtail.com>
2024-06-25 11:10:31 +0100

    added up, down and left to swtich operator in move function, also added unshift and pop functions

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const board = document.getElementById('game-board');
 
 // Define game variables
 const gridSize = 20
-let snake = [{x:10, y:10}];
+let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
 let direction = 'right';
 
 // Adds game map, snake and food
-function draw () {
+function draw() {
     board.innerHTML = ''; // resets board to empty
     drawSnake();
     drawFood();
@@ -36,15 +36,15 @@ function setPosition(element, position) {
 }
 
 // Testing draw function 
-  draw();
+draw();
 
 // Draw Food function 
 function drawFood() {
-    
-        const foodElement = createGameElement('div', 'food');
-        setPosition(foodElement, food);
-        board.appendChild(foodElement);
-    
+
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+
 }
 
 //Randomly generated food position in x and y using maths random and * gridsize to spawn food anywhere in 20x20 grid
@@ -54,15 +54,32 @@ function generateFood() {
     return { x, y };
 }
 
-// Snake Movement using shallow copy, not altering the orginial array. Spread operator 0 to get a hold of snakes first postion. 
-function move () {
-    const head = {...snake[0]};
+// Snake Movement using shallow copy, not altering the orginial array. 
+function move() {
+    const head = { ...snake[0] }; //Spread operator 0 to get a hold of snakes first postion and copies it.
     switch (direction) {
-        case value:
-            
+        case 'up':
+            head.y--; // the direction of the snake head
             break;
-    
-        default:
+        case 'down':
+            head.y++; 
             break;
+        case 'left':
+            head.x--; 
+            break;
+        case 'right':
+            head.x++; 
+            break;
+
     }
+
+    snake.unshift (head); // this adds another head element onto the current one
+
+    snake.pop(); // this removes the last element from the array. Which is the unshift.
 }
+
+// test move
+// setInterval(() => {
+//     move(); //Move first
+//     draw(); // draw a new position
+// }, 200)
