dceb13934e049b8a34dc6d71f0f0a7722a9e9296
lochy2000 <lochlann_oht@homtail.com>
2024-06-26 13:41:59 +0100

    trying to add touch controls for a phone

--- script.js
+++ script.js
@@ -261,3 +261,50 @@ function updateHighScore() {
     }
     highScoreText.style.display = 'block';
 }
+
+// Event listener for touch controls for phone
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+    return evt.touches || evt.originalEvent.touches;
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    const xUp = evt.touches[0].clientX;
+    const yUp = evt.touches[0].clientY;
+
+    const xDiff = xDown - xUp;
+    const yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        // Most significant
+        if (xDiff > 0) {
+            direction = 'left';
+        } else {
+            direction = 'right';
+        }
+    } else {
+        if (yDiff > 0) {
+            direction = 'up';
+        } else {
+            direction = 'down';
+        }
+    }
+    // Reset values
+    xDown = null;
+    yDown = null;
+}
